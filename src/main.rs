135a93de5e0c47f7abd47e7e19c3259e87d7be8b
tedirland <irlandth@gmail.com>
2023-05-03 10:50:39 -0500

    Added logic to track and reset score/high score

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 use rusty_engine::prelude::*;
 
 struct GameState {
-    // high_score: u32,
-    current_score: u32,
+    high_score: u32,
+    score: u32,
     sweatpants_index: i32,
     // spawn_timer: Timer,
 }
@@ -11,8 +11,8 @@ struct GameState {
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            // high_score: 0,
-            current_score: 0,
+            high_score: 0,
+            score: 0,
             sweatpants_index: 0,
             // spawn_timer: Timer::from_seconds(1.0, false),
         }
@@ -28,6 +28,12 @@ fn main() {
     player.rotation = SOUTH_WEST;
     player.scale = 1.0;
     player.collision = true;
+
+    let score = game.add_text("score", "Score: 0");
+    score.translation = Vec2::new(520.0, 320.0);
+
+    let high_score = game.add_text("high_score", "High Score: 0");
+    high_score.translation = Vec2::new(-520.0, 320.0);
 
     // setup game here
     game.add_logic(game_logic);
@@ -45,8 +51,14 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                     engine.sprites.remove(&label);
                 }
             }
-            game_state.current_score += 1;
-            println!("Current score {}", game_state.current_score);
+            game_state.score += 1;
+            let score = engine.texts.get_mut("score").unwrap();
+            score.value = format!("Score {}", game_state.score);
+            if game_state.score > game_state.high_score {
+                game_state.high_score = game_state.score;
+                let high_score = engine.texts.get_mut("high_score").unwrap();
+                high_score.value = format!("High Score: {}", game_state.score);
+            }
         }
     }
 
@@ -82,7 +94,6 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     }
 
     // handle mouse input
-
     if engine.mouse_state.just_pressed(MouseButton::Left) {
         if let Some(mouse_location) = engine.mouse_state.location() {
             let label = format!("drsweatpants{}", game_state.sweatpants_index);
@@ -92,5 +103,13 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
             dsp.collision = true;
             dsp.scale = 4.0;
         }
+    }
+
+    // Reset Score
+
+    if engine.keyboard_state.just_pressed(KeyCode::R) {
+        let score = engine.texts.get_mut("score").unwrap();
+        game_state.score = 0;
+        score.value = format!("Score {}", game_state.score);
     }
 }
